50d82fb20cfdd19c5a5870fe7f130d9bf37778c6
Tanmoy Roy <royanupama782@gmail.com>
2022-12-15 00:15:27 +0530

    Stracture Of landing Page

---
+++ script/index.js
@@ -0,0 +1,4 @@
+console.log("hellow world");
+
+let topBanner = document.getElementById("top_banner");
+let bannerImmage = ["image/top_banner/1c360d69-811d-4e8b-8e6a-0ff953b512a7.avif", "image/top_banner/41cab243-d530-4f83-b083-6352b09f3a13.avif,/image/top_banner/7250c4ef-daf6-4f77-ab9d-ea897b773904.avif,/image/top_banner/ae238ad7-9d3a-48ec-b143-a9af8e1f1a0c.avif,/image/top_banner/db2d5607-d968-45f6-ba39-6b8eac6a54e4.avif,/image/top_banner/fe616105-d856-4ef7-9a91-22a68a988094.avif"]
